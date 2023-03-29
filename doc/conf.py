b2b8c4e2f015d1e932dde1a5102349f0b7046c0a
Regen <regen100@users.noreply.github.com>
2023-03-29 13:00:40 +0900

    Add intersphinx

--- doc/conf.py
+++ doc/conf.py
@@ -5,8 +5,13 @@ author = "Regen"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
 ]
 
 html_theme = "sphinx_rtd_theme"
 
 autodoc_member_order = "bysource"
+
+intersphinx_mapping = {
+    "onnx": ("https://onnx.ai/onnx/", None),
+}
